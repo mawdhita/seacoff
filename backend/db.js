87caa43d320ca8bcb665d5a97c7e511c9b33eae5
Mawar Dhita <149513147+mawdhita@users.noreply.github.com>
2025-06-03 05:51:38 -0700

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,29 +1,14 @@
 const mysql = require('mysql2');
-const url = require('url');
 
-// Railway recommended: use MYSQL_URL environment variable
-const dbUrl = process.env.MYSQL_URL || 'mysql://root:MVLuTixdZImaYvpJVKpGqOFZFZxedKxQ@mysql.railway.internal:3306/railway';
+// Gunakan URL koneksi dari Railway
+const connection = mysql.createConnection('mysql://root:MVLuTixdZImaYvpJVKpGqOFZFZxedKxQ@switchback.proxy.rlwy.net:23941/railway');
 
-const parsedUrl = url.parse(dbUrl);
-const [user, password] = parsedUrl.auth.split(':');
-
-const db = mysql.createConnection({
-    host: parsedUrl.hostname,
-    user: user,
-    password: password,
-    database: parsedUrl.pathname.replace('/', ''),
-    port: parsedUrl.port,
-    ssl: {
-        rejectUnauthorized: false
-    }
-});
-
-db.connect(err => {
+connection.connect(err => {
     if (err) {
         console.error('Error connecting to database:', err);
     } else {
-        console.log('Connected to MySQL database');
+        console.log('Connected to MySQL database railway');
     }
 });
 
-module.exports = db;
+module.exports = connection;
