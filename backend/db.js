b7a8a33d1cee633c7c6e10c1b94d4a8bd9466f2a
Mawar Dhita <149513147+mawdhita@users.noreply.github.com>
2025-06-03 05:42:02 -0700

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,17 +1,21 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '', // lo bilang tadi ga pakai password
-    database: 'balen_coffee'
+    host: process.env.MYSQLHOST || 'mysql.railway.internal',
+    user: process.env.MYSQLUSER || 'root',
+    password: process.env.MYSQLPASSWORD || 'MVLuTixdZImaYvpJVKpGqOFZFZxedKxQ',
+    database: process.env.MYSQLDATABASE || 'railway',
+    port: process.env.MYSQLPORT || 3306,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 db.connect(err => {
     if (err) {
-        console.error('Error connect ke database:', err);
+        console.error('Error connecting to database:', err);
     } else {
-        console.log('Connected ke MySQL database balen_coffee');
+        console.log('Connected to MySQL database');
     }
 });
 
